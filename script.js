f6ca15b86333f76830b09e247b4d3300c38deaec
Trey <contacttreyw@gmail.com>
2023-10-30 20:44:48 -0500

    Part two

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $.ajax({
+      type: 'GET',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<p>' + task.content + '</p>');
+        })
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  });
