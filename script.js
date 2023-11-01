f54c01356a702aa2c7e52a33b304412da7802bfa
Trey <contacttreyw@gmail.com>
2023-11-01 18:21:20 -0500

    Psrt 4 publishing to to the Page

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function(){
-  console.log('dom ready');
-  
   $.ajax({
     type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1075',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
     dataType: 'json',
     success: function (response, textStatus) {
-      console.log(response);
+      response.tasks.forEach(function (task) {
+        $('#todo-list').append('<p>' + task.content + '</p>');
+      })
     },
     error: function (request, textStatus, errorMessage) {
       console.log(errorMessage);
