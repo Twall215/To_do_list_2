8be4ecf67bb7c934442cb77af5cba69b4084ac67
Trey <contacttreyw@gmail.com>
2023-11-01 18:17:49 -0500

    Part 3

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
-    $.ajax({
-      type: 'GET',
-      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
-      dataType: 'json',
-      success: function (response, textStatus) {
-        response.tasks.forEach(function (task) {
-          $('#todo-list').append('<p>' + task.content + '</p>');
-        })
-      },
-      error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
+  console.log('dom ready');
+  
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1075',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
   });
+});
